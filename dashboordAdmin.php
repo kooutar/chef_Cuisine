7bc58e5e338bb7b9c9cf679e71b7135246151035
kaoutar laajil <kawtarlaajil7@gmail.com>
2024-12-17 23:12:42 +0100

    page Client Version 1

--- dashboordAdmin.php
+++ dashboordAdmin.php
@@ -36,7 +36,7 @@
 .btn {
     padding: 10px;
     margin-bottom: 8px;
-    background-color: #385c90;
+    background-color:  #f34949;
     color: #fff;
     border: none;
     border-radius: 6px;
@@ -56,17 +56,17 @@
     <div class="flex flex-col">
         <!-- Partie Statistique -->
         <main class="p-6">
-            <div id="partieStatistique" class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 justify-center">
-                <div id="statiqueCleint" class="w-full sm:w-40 h-20 bg-red-500 text-white rounded-lg shadow-md flex justify-center items-center">
+            <div id="partieStatistique" class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 md:pl-12">
+                <div id="statiqueCleint" class="w-full sm:w-40 h-20 bg-green-500 text-white rounded-lg shadow-md flex justify-center items-center">
                     <p class="font-medium text-sm uppercase tracking-wider">0 client</p>
                 </div>
-                <div id="statiqueReservationAccepter" class="w-full sm:w-40 h-20 p-6 bg-green-500 text-white rounded-lg shadow-md flex justify-center items-center">
+                <div id="statiqueReservationAccepter" class="w-full sm:w-40 h-20 p-6 bg-yellow-500 text-white rounded-lg shadow-md flex justify-center items-center">
                     <p class="font-medium text-sm uppercase tracking-wider">0 Reservation acceptée</p>
                 </div>
                 <div id="statiqtiqueresevationRefuser" class="w-full sm:w-40 h-20 p-6 bg-blue-500 text-white rounded-lg shadow-md flex justify-center items-center">
                     <p class="font-medium text-sm uppercase tracking-wider">0 Reservation refusée</p>
                 </div>
-                <div id="statiqtiqueresevationEnAttent" class="w-full sm:w-40 h-20  bg-yellow-500 text-white rounded-lg shadow-md flex justify-center items-center">
+                <div id="statiqtiqueresevationEnAttent" class="w-full sm:w-40 h-20  bg-red-500 text-white rounded-lg shadow-md flex justify-center items-center">
                     <p class="font-medium text-sm uppercase tracking-wider">0 En attente</p>
                 </div>
             </div>
@@ -76,18 +76,19 @@
         <section class="flex flex-col md:flex-row gap-6 p-6">
             <!-- Sidebar -->
             <div id="partiegestionDash" class="grid grid-cols-2 md:flex md:flex-col gap-4 p-6 bg-white shadow-lg rounded-lg md:w-1/4 w-full">
-                <button class="btn w-full">Ajouter menu</button>
-                <button class="btn w-full">Ajout plat</button>
-                <button class="btn w-full">Voir tous les clients</button>
-                <button class="btn w-full">Demande acceptée</button>
-                <button class="btn w-full">Demande refusée</button>
-                <button class="btn w-full">Demande en attente</button>
-                <button class="btn w-full">Modifier menu</button>
+                <button id="bteAjoutMenu" class="btn w-full">Ajouter menu</button>
+                <button id="bteAjoutPlate" class="btn w-full">Ajout plate</button>
+                <button id="bteAllCleints" class="btn w-full">Voir tous les clients</button>
+                <button id="bteAcceptedDemande"class="btn w-full">Demande acceptée</button>
+                <button id="bteDemandeRefuse"class="btn w-full">Demande refusée</button>
+                <button id="bteDemandeAttente"class="btn w-full">Demande en attente</button>
+                <button id="bteEditMenu"class="btn w-full">Modifier menu</button>
             </div>
 
             <!-- Content -->
             <div id="contentDash" class="flex-1 bg-white shadow-lg rounded-lg p-6 overflow-x-auto">
-               <div id="tableUserAttente" class="hidden">
+                <!-- table user en attente -->
+               <div id="tableUserAttente" class="section ">
                 <h6 class="font-medium text-lg uppercase tracking-wider mb-4 text-gray-700">Réservation en attente</h6>
                 <table id="tableClients" class="table-auto min-w-max w-full bg-gray-50 border border-gray-300 rounded-lg shadow-md">
                     <thead>
@@ -114,8 +115,9 @@
                     </tbody>
                 </table>
             </div>
-            <!-- ************************************ -->
-             <div id="formPlat">
+            <!-- *****************formulaire ajout plate******************* -->
+
+    <div id="formPlat" class="section hidden">
              <div class="flex flex-col items-center justify-center min-h-screen p-6">
         <!-- Conteneur principal -->
         <div class="w-full max-w-4xl bg-white shadow-lg rounded-lg p-8">
@@ -207,12 +209,161 @@
                 </div>
             </form>
         </div>
-    </div>
+       </div>
 
-             </div>
+    </div>
+    <!-- **********************table user accepter************************* -->
+    <div id="tableUserAccepter" class="section hidden">
+                <h6 class="font-medium text-lg uppercase tracking-wider mb-4 text-gray-700">Réservation Acceptées</h6>
+                <table id="tableClients" class="table-auto min-w-max w-full bg-gray-50 border border-gray-300 rounded-lg shadow-md">
+                    <thead>
+                        <tr class="bg-gray-200 text-gray-600">
+                            <th class="py-3 px-4 font-medium uppercase">Nom Menu</th>
+                            <th class="py-3 px-4 font-medium uppercase">Mail User</th>
+                            <th class="py-3 px-4 font-medium uppercase">Date de réservation</th>
+                            <th class="py-3 px-4 font-medium uppercase">Heure</th>
+                            <th class="py-3 px-4 font-medium uppercase">Nombre de personnes</th>
+                            <th class="py-3 px-4 font-medium uppercase">Statut</th>
+                            <th class="py-3 px-4 font-medium uppercase">Edit/Suppression</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="hover:bg-gray-100">
+                            <td class="py-3 px-4 text-gray-700">{$client['id_client']}</td>
+                            <td class="py-3 px-4 text-gray-700">{$client['nom']}</td>
+                            <td class="py-3 px-4 text-gray-700">{$client['pernom']}</td>
+                            <td class="py-3 px-4 text-gray-700">{$client['email']}</td>
+                            <td class="py-3 px-4 text-gray-700">{$client['telephone']}</td>
+                            <td class="py-3 px-4 text-gray-700">{$client['adresse']}</td>
+                            <td class="py-3 px-4 text-gray-700">{$client['date_naissance']}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <!-- *********************demande refusee -->
+              <!-- **********************table user accepter************************* -->
+    <div id="tableUserRefusee" class="section hidden">
+                <h6 class="font-medium text-lg uppercase tracking-wider mb-4 text-gray-700">Réservation refusée</h6>
+                <table id="tableClients" class="table-auto min-w-max w-full bg-gray-50 border border-gray-300 rounded-lg shadow-md">
+                    <thead>
+                        <tr class="bg-gray-200 text-gray-600">
+                            <th class="py-3 px-4 font-medium uppercase">Nom Menu</th>
+                            <th class="py-3 px-4 font-medium uppercase">Mail User</th>
+                            <th class="py-3 px-4 font-medium uppercase">Date de réservation</th>
+                            <th class="py-3 px-4 font-medium uppercase">Heure</th>
+                            <th class="py-3 px-4 font-medium uppercase">Nombre de personnes</th>
+                            <th class="py-3 px-4 font-medium uppercase">Statut</th>
+                            <th class="py-3 px-4 font-medium uppercase">Edit/Suppression</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="hover:bg-gray-100">
+                            <td class="py-3 px-4 text-gray-700">{$client['id_client']}</td>
+                            <td class="py-3 px-4 text-gray-700">{$client['nom']}</td>
+                            <td class="py-3 px-4 text-gray-700">{$client['pernom']}</td>
+                            <td class="py-3 px-4 text-gray-700">{$client['email']}</td>
+                            <td class="py-3 px-4 text-gray-700">{$client['telephone']}</td>
+                            <td class="py-3 px-4 text-gray-700">{$client['adresse']}</td>
+                            <td class="py-3 px-4 text-gray-700">{$client['date_naissance']}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+              <!-- ***************Voir All clients********************* -->
+              <div id="tableAllUser" class="section hidden">
+                <h6 class="font-medium text-lg uppercase tracking-wider mb-4 text-gray-700">Tous les cleints</h6>
+                <table id="tableClients" class="table-auto min-w-max w-full bg-gray-50 border border-gray-300 rounded-lg shadow-md">
+                    <thead>
+                        <tr class="bg-gray-200 text-gray-600">
+                            <th class="py-3 px-4 font-medium uppercase">Nom Menu</th>
+                            <th class="py-3 px-4 font-medium uppercase">Mail User</th>
+                            <th class="py-3 px-4 font-medium uppercase">Date de réservation</th>
+                            <th class="py-3 px-4 font-medium uppercase">Heure</th>
+                            <th class="py-3 px-4 font-medium uppercase">Nombre de personnes</th>
+                            <th class="py-3 px-4 font-medium uppercase">Statut</th>
+                            <th class="py-3 px-4 font-medium uppercase">Edit/Suppression</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="hover:bg-gray-100">
+                            <td class="py-3 px-4 text-gray-700">{$client['id_client']}</td>
+                            <td class="py-3 px-4 text-gray-700">{$client['nom']}</td>
+                            <td class="py-3 px-4 text-gray-700">{$client['pernom']}</td>
+                            <td class="py-3 px-4 text-gray-700">{$client['email']}</td>
+                            <td class="py-3 px-4 text-gray-700">{$client['telephone']}</td>
+                            <td class="py-3 px-4 text-gray-700">{$client['adresse']}</td>
+                            <td class="py-3 px-4 text-gray-700">{$client['date_naissance']}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
             </div>
         </section>
+    
+   
     </div>
+    <script>
+        const AllSection=document.querySelectorAll('.section');
+        document.querySelector('#bteAjoutPlate').addEventListener('click',()=>{
+            AllSection.forEach(section=>{
+            if(section.id!="formPlat")
+            {
+                section.classList.add('hidden')
+            }else{
+                section.classList.remove('hidden')
+            }
+            })
+
+        })
+        document.querySelector('#bteDemandeAttente').addEventListener('click',()=>{
+            AllSection.forEach(section=>{
+            if(section.id!="tableUserAttente")
+            {
+                section.classList.add('hidden')
+            }else{
+                section.classList.remove('hidden')
+            }
+            })
+
+        })
+        document.querySelector('#bteAcceptedDemande').addEventListener('click',()=>{
+            AllSection.forEach(section=>{
+            if(section.id!="tableUserAccepter")
+            {
+                section.classList.add('hidden')
+            }else{
+                section.classList.remove('hidden')
+            }
+            })
+
+        })
+        document.querySelector('#bteDemandeRefuse').addEventListener('click',()=>{
+            AllSection.forEach(section=>{
+            if(section.id!="tableUserRefusee")
+            {
+                section.classList.add('hidden')
+            }else{
+                section.classList.remove('hidden')
+            }
+            })
+
+        })
+       
+        document.querySelector('#bteAllCleints').addEventListener('click',()=>{
+            AllSection.forEach(section=>{
+            if(section.id!="tableAllUser")
+            {
+                section.classList.add('hidden')
+            }else{
+                section.classList.remove('hidden')
+            }
+            })
+
+        })
+        
+    </script>
 </body>
 
 
---
+++ pageClient.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body style="background-color:bisque;">
+    <section class="md:m-6 p-2" style="background-color:whitesmoke;">
+     <nav class="flex justify-around">
+      <img src="img/logo.png" alt="" srcset="" class="w-16">
+      <p>kaoutar</p>
+     </nav>
+     <main class="flex flex-col">
+        <div class="flex justify-center">
+            <h1>Our Menu</h1>
+        </div>
+        <div class="grid grid-cols-2 gap-6 md:grid-cols-4 md:gap-8">
+        <div
+  class="flex flex-col rounded-lg bg-white text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white md:max-w-xl md:flex-row">
+  <img
+    class="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:!rounded-none md:!rounded-s-lg"
+    src="img/salmon-zucchini.jpg"
+    alt="" />
+  <div class="flex flex-col justify-start p-6">
+    <h5 class="mb-2 text-xl font-medium">Card title</h5>
+    <p class="mb-4 text-base">
+      This is a wider card with supporting text below as a natural lead-in
+      to additional content. This content is a little bit longer.
+    </p>
+    <p class="text-xs text-surface/75 dark:text-neutral-300">
+      Last updated 3 mins ago
+    </p>
+  </div>
+</div>
+
+<div
+  class="flex flex-col rounded-lg bg-white text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white md:max-w-xl md:flex-row">
+  <img
+    class="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:!rounded-none md:!rounded-s-lg"
+    src="img/salmon-zucchini.jpg"
+    alt="" />
+  <div class="flex flex-col justify-start p-6">
+    <h5 class="mb-2 text-xl font-medium">Card title</h5>
+    <p class="mb-4 text-base">
+      This is a wider card with supporting text below as a natural lead-in
+      to additional content. This content is a little bit longer.
+    </p>
+    <p class="text-xs text-surface/75 dark:text-neutral-300">
+      Last updated 3 mins ago
+    </p>
+  </div>
+</div>
+
+<div
+  class="flex flex-col rounded-lg bg-white text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white md:max-w-xl md:flex-row">
+  <img
+    class="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:!rounded-none md:!rounded-s-lg"
+    src="img/salmon-zucchini.jpg"
+    alt="" />
+  <div class="flex flex-col justify-start p-6">
+    <h5 class="mb-2 text-xl font-medium">Card title</h5>
+    <p class="mb-4 text-base">
+      This is a wider card with supporting text below as a natural lead-in
+      to additional content. This content is a little bit longer.
+    </p>
+    <p class="text-xs text-surface/75 dark:text-neutral-300">
+      Last updated 3 mins ago
+    </p>
+  </div>
+</div>
+
+<div
+  class="flex flex-col rounded-lg bg-white text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white md:max-w-xl md:flex-row">
+  <img
+    class="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:!rounded-none md:!rounded-s-lg"
+    src="img/salmon-zucchini.jpg"
+    alt="" />
+  <div class="flex flex-col justify-start p-6">
+    <h5 class="mb-2 text-xl font-medium">Card title</h5>
+    <p class="mb-4 text-base">
+      This is a wider card with supporting text below as a natural lead-in
+      to additional content. This content is a little bit longer.
+    </p>
+    <p class="text-xs text-surface/75 dark:text-neutral-300">
+      Last updated 3 mins ago
+    </p>
+  </div>
+</div>
+        </div>
+
+     </main>
+    </section>
+</body>
+</html>
